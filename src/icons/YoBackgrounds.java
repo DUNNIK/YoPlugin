fc7b4507be1eaa6d407b3e07ce88fc554f0bd9fe
DUNNIK <dns.56@mail.ru>
2021-06-21 13:57:14 +0300

    clean up

--- src/icons/YoBackgrounds.java
+++ src/icons/YoBackgrounds.java
@@ -10,23 +10,26 @@ import static javax.imageio.ImageIO.read;
 
 public class YoBackgrounds {
     private static final String WALL_BACKGROUND_PATH = "/Backgrounds/bricks.png";
-    private static final String GRASS_BACKGROUND_PATH = "/Backgrounds/grass.png";
-    private static final String MOUNTAIN_BACKGROUND_PATH = "/Backgrounds/mountains.png";
     private static final String RAINBOW_BACKGROUND_PATH = "/Backgrounds/rainbow.png";
     private static final String GREY_BACKGROUND_PATH = "/Backgrounds/gray.png";
-    private static final String LIGHTGREY_BACKGROUND_PATH = "/Backgrounds/lightGray.png";
-    private static final String DEADPOOL_BACKGROUND_PATH = "/Backgrounds/deadpoolBackground.png";
     private static final String SONIC_BACKGROUND_PATH = "/Backgrounds/sonicBackground.png";
-    private static final String HOMER_BACKGROUND_PATH = "/Backgrounds/пончики.png";
-    private static final String DARK_FOREST_BACKGROUND_PATH = "/Backgrounds/darkForest.png";
-    private static final String LIGHT_FOREST_BACKGROUND_PATH = "/Backgrounds/lightForest.png";
-    private static final String GREY_WALL_BACKGROUND_PATH = "/Backgrounds/wall.png";
     private static final String MINECRAFT_BACKGROUND_PATH = "/Backgrounds/minecraft.png";
-    private static final String CAVE_BACKGROUND_PATH = "/Backgrounds/cave.png";
     private static final String NARUTO_BACKGROUND_PATH = "/Backgrounds/Naruto.png";
     private static final String NARUTO_YELLOW_BACKGROUND_PATH = "/Backgrounds/narutoYellow.png";
 
     private YoBackgrounds() { throw new IllegalStateException("Utility class"); }
+
+    public static String getWallBackgroundPath() {
+        return WALL_BACKGROUND_PATH;
+    }
+
+    public static String getSonicBackgroundPath() {
+        return SONIC_BACKGROUND_PATH;
+    }
+
+    public static String getRainbowBackgroundPath() {
+        return RAINBOW_BACKGROUND_PATH;
+    }
 
     public static String getNarutoYellowBackgroundPath() {
         return NARUTO_YELLOW_BACKGROUND_PATH;
@@ -36,61 +39,14 @@ public class YoBackgrounds {
         return NARUTO_BACKGROUND_PATH;
     }
 
-    public static String getCaveBackgroundPath() {
-        return CAVE_BACKGROUND_PATH;
-    }
-
     public static String getMinecraftBackgroundPath() {
         return MINECRAFT_BACKGROUND_PATH;
-    }
-
-    public static String getGreyWallBackgroundPath() {
-        return GREY_WALL_BACKGROUND_PATH;
-    }
-
-    public static String getLightForestBackgroundPath() {
-        return LIGHT_FOREST_BACKGROUND_PATH;
-    }
-
-    public static String getDarkForestBackgroundPath() {
-        return DARK_FOREST_BACKGROUND_PATH;
-    }
-
-    public static String getHomerBackgroundPath() {
-        return HOMER_BACKGROUND_PATH;
-    }
-
-    public static String getDeadpoolBackgroundPath() {
-        return DEADPOOL_BACKGROUND_PATH;
-    }
-
-    public static String getSonicBackgroundPath() {
-        return SONIC_BACKGROUND_PATH;
-    }
-
-    public static String getWallBackgroundPath() {
-        return WALL_BACKGROUND_PATH;
-    }
-
-    public static String getMountainBackgroundPath() {
-        return MOUNTAIN_BACKGROUND_PATH;
-    }
-
-    public static String getGrassBackgroundPath() {
-        return GRASS_BACKGROUND_PATH;
-    }
-
-    public static String getRainbowBackgroundPath() {
-        return RAINBOW_BACKGROUND_PATH;
     }
 
     public static String getGreyBackgroundPath() {
         return GREY_BACKGROUND_PATH;
     }
 
-    public static String getLightgreyBackgroundPath() {
-        return LIGHTGREY_BACKGROUND_PATH;
-    }
     private static BufferedImage resizeImage(BufferedImage image) {
         return Scalr.resize(image, 32);
     }
--- src/icons/YoIcons.java
+++ src/icons/YoIcons.java
@@ -8,26 +8,16 @@ import java.net.URL;
 
 public class YoIcons {
     private static final String MARIO_PATH = "/Gifs/mario.gif";
-    private static final String DEADPOOL_PATH = "/Gifs/deadpool.gif";
     private static final String SONIC_PATH = "/Gifs/соник.gif";
-    private static final String ZOMBIE_PATH = "/Gifs/зомби.gif";
-    private static final String PIKACHU_PATH = "/Gifs/picachu.gif";
-    private static final String SKELETONS_PATH = "/Gifs/Скелеты.gif";
     private static final String NYAN_CAT_PATH = "/Gifs/NyanCat.gif";
-    private static final String HOMER_PATH = "/Gifs/simpson.gif";
     private static final String NARUTO_PATH = "/Gifs/narutoRun.gif";
     private static final String MINECRAFT_PATH = "/Gifs/minecraft.gif";
-    private static final String TRAVELER_PATH = "/Gifs/traveler.gif";
     private static final String SASUKE_PATH = "/Gifs/saske.gif";
 
-    public YoIcons() {throw new IllegalStateException("Utility class"); }
+    private YoIcons() {throw new IllegalStateException("Utility class"); }
 
     public static String getSasukePath() {
         return SASUKE_PATH;
-    }
-
-    public static String getTravelerPath() {
-        return TRAVELER_PATH;
     }
 
     public static String getMinecraftPath() {
@@ -38,38 +28,17 @@ public class YoIcons {
         return NARUTO_PATH;
     }
 
-    public static String getDeadpoolPath() {
-        return DEADPOOL_PATH;
-    }
-
-    public static String getHomerPath() {
-        return HOMER_PATH;
-    }
-
-    public static String getZombiePath() {
-        return ZOMBIE_PATH;
+    public static String getMarioPath() {
+        return MARIO_PATH;
     }
 
     public static String getSonicPath() {
         return SONIC_PATH;
     }
 
-    public static String getSkeletonsPath() {
-        return SKELETONS_PATH;
-    }
-
-    public static String getPikachuPath() {
-        return PIKACHU_PATH;
-    }
-
     public static String getNyanCatPath() {
         return NYAN_CAT_PATH;
     }
-
-    public static String getMarioPath() {
-        return MARIO_PATH;
-    }
-
 
     @NotNull
     @Contract("_ -> new")
